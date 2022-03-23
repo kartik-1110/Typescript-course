d9d091ea55c3fbe541cdfa3b3e5f314a00262565
Kartik Pal <kartik.p1110@gmail.com>
2022-03-23 17:23:07 +0530

    final commit

--- src/main.ts
+++ src/main.ts
@@ -44,3 +44,66 @@ const dragonTags: MaybePopularTag = null;
 const doSomething = (): void => {
   console.log("do something");
 };
+
+// any
+// any type turns off typescript checks
+let foo: any = "foo";
+console.log(foo.bar());
+
+// never
+// function with "never" cant be executed to the end
+// function that returns "never" , never ends
+const doSomething2 = (): never => {
+  console.log("do something 2");
+};
+const doSomething3 = (): never => {
+  throw "never";
+};
+
+// unknown
+//  we can assign unknown directly in the other types
+let vAny: any = 10;
+let vUnknown: unknown = 10;
+let s1: string = vAny;
+// let s2: string = vUnknown
+// unknown is not assignable to a string
+
+// assertion
+// we want to convert one type to another
+//  "as" operator makes type assertion
+let g1: string = vAny;
+let g2: string = vUnknown as string;
+
+// Working with DOM
+
+// this has type: Element
+// Element is the highest class in heirarchy
+const someElement = document.querySelector(".foo");
+console.log("some element ", someElement);
+
+// Event listeners
+// event also has highest class in heirarchy
+const someAthorElement = document.querySelector(".foo");
+someAthorElement.addEventListener("blur", (e) => {
+  const target = e.target as HTMLInputElement;
+  console.log("event", target.value);
+});
+
+//Classes in typescript
+// everything is public by default
+// private means we can use only inside a class
+// Protected is allowed only in class and children
+// if you want ot use const in class use word "readonly"
+class ClassUser {
+  private firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  getFullName(): string {
+    return this.firstName + " " + this.lastName;
+  }
+}
+const classUser = new ClassUser("Alex", "Ferguson");
